f1f5d5d22f6adfceb37240b47ab0bf7ae24a979e
kowsalyadevi12 <kowsiosundhar12@gmail.com>
2023-06-26 10:09:01 +0530

    added blog post code

---
+++ home/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+from .views import UpdatePostView
+
+urlpatterns = [
+#     blogs
+    path("", views.blogs, name="blogs"),
+    path("blog/<str:slug>/", views.blogs_comments, name="blogs_comments"),
+    path("add_blogs/", views.add_blogs, name="add_blogs"),
+    path("edit_blog_post/<str:slug>/", UpdatePostView.as_view(), name="edit_blog_post"),
+    path("delete_blog_post/<str:slug>/", views.Delete_Blog_Post, name="delete_blog_post"),
+
+    
+    
+#    user authentication
+    path("register/", views.Register, name="register"),
+    path("login/", views.Login, name="login"),
+    path("logout/", views.Logout, name="logout"),
+]
